6d534ef04db8aace33d62c3d005d955b4b828730
Amy Spears <amspears007@gmail.com>
2023-07-21 09:36:52 -0600

    Add controller with faraday connection and response and view page creation

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,15 @@
+class FoodsController < ApplicationController
+  def index
+    
+    food = params[:q]
+    
+    conn = Faraday.new(url: "https://api.nal.usda.gov/") do |faraday|
+      faraday.headers["X-Api-Key"] = ENV["api_key"]
+    end
+    
+    response = conn.get("fdc/v1/foods/search?query=#{food}&pageSize=10&pageNumber=1")
+    @food_info = JSON.parse(response.body, symbolize_names: true)[:foods]
+    
+    require 'pry'; binding.pry
+  end
+end
