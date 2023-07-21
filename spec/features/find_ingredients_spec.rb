e5af5d9cfd1553050084997e3949a2b2e1b2b8b4
Amy Spears <amspears007@gmail.com>
2023-07-21 10:05:32 -0600

    Rendered descripiton ingredients and brand owner and search count on page with format

--- spec/features/find_ingredients_spec.rb
+++ spec/features/find_ingredients_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
 RSpec.describe "Ingredient Search Page" do
-  it "When I visi the page the user submits a valid state search" do
-    visit "/"
+  it "When I visit the page the user submits a valid state search" do
+    visit root_path
 
     fill_in "q", with: "sweet potatoes"
-      click_on "Search"
-
-      expect(current_path).to eq(ingredient_search_results_path)
+    click_on "Search"
+save_and_open_page
+      expect(current_path).to eq(foods_path)
       expect(page).to have_content("SWEET POTATOES")
       expect(page).to have_content("NOT A BRANDED ITEM")
       expect(page).to have_content("ORGANIC SWEET POTATOES.")
