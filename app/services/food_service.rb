b545b0f013190ce6a3907f5b76ccf372d2fff787
Amy Spears <amspears007@gmail.com>
2023-07-29 10:23:16 -0600

    break up service method into conn, get_url and specific search so each method only does one job

--- app/services/food_service.rb
+++ app/services/food_service.rb
@@ -1,12 +1,17 @@
 class FoodService
 
-  def food_by_name(food)
+  def food_by_name(food)    
+    get_url("fdc/v1/foods/search?query=#{food}&pageSize=10&pageNumber=1")
+  end 
+  
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
-    conn = Faraday.new(url: "https://api.nal.usda.gov/") do |faraday|
+  def conn
+    Faraday.new(url: "https://api.nal.usda.gov/") do |faraday|
       faraday.headers["X-Api-Key"] = ENV["api_key"]
     end
-    
-    response = conn.get("fdc/v1/foods/search?query=#{food}&pageSize=10&pageNumber=1")
-    @food_info = JSON.parse(response.body, symbolize_names: true)
-  end 
+  end
 end
